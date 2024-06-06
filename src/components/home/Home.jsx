2b4896d00a6a77ae7c037149d8de406b522927cd
Abhishek <abhishekonline45@gmail.com>
2024-06-06 20:38:03 +0530

    first commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from './hero/Hero';
 import HAbout from './HAbout';
+import AboutCard from '../about/AboutCard';
+import Test from './testimonal/Test';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <AboutCard />
       <HAbout />
+      <Test/>
     </div>
   )
 };
---
+++ src/components/pricing/Faq.jsx
@@ -0,0 +1,39 @@
+import React, {useState }from 'react';
+import Title from '../common/title/Title';
+import { faq } from '../../dummydata';
+
+
+const Faq = () => {
+    // ADDING FAQ ONCLICK VIEW AND HIDE
+    
+    const [click, setClick] = useState(false);
+    const toggle = (index) => {
+        if (click === index) {
+          return setClick(null)
+        }
+        setClick(index)
+      }
+
+  return (
+    <>
+      <Title subtitle="FAQS" title="Frequently Asked Questions" />
+      <section className='faq'>
+         <div className="container">
+            {faq.map((val, index) => (
+                <div className="box">
+                    <button className="accordion" onClick={() => toggle(index)} key={index}>
+                       <h2>{val.title}</h2> 
+                    <span>{click === index ? <i className='fa fa-chevron-down'></i> : <i className='fa fa-chevron-right'></i>}</span>   
+                    </button>
+                   {click === index ?(<div className="text">
+                       <p>{val.desc}</p> 
+                    </div>) : null} 
+                </div>
+            ))}
+         </div>
+      </section>
+    </>
+  )
+};
+
+export default Faq;
