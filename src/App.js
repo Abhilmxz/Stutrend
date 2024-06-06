4619c1e127e5b030ee3f7b9a01feac8e288b6dbb
Abhishek <abhishekonline45@gmail.com>
2024-06-06 15:46:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import Header from './components/common/header/Header';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./components/home/Home";
 import About from './components/about/About';
-// import Hero from './components/home/hero/Hero';
-
 
 const App = () => {
   return (
-    <>
-      <Router>
+    <Router>
       <Header />
-      {/* <Hero/>
-      <About/> */}
       <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path="/about" exact component={About} />
+        <Route path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-      </Router>
-    </>
-  )
+    </Router>
+  );
 };
 
 export default App;
